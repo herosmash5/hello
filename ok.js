97c43bc4ce7ae0d43ed66ebf9797f8a9c4c45662
herosmash5 <pranavsehgal91@yahoo.com>
2014-06-12 23:52:02 +0530

    Update ok.js

--- ok.js
+++ ok.js
@@ -1,8 +1,17 @@
+var a = document.getElementById("swag").value;
+var ad = document.getElementById("swag").defaultvalue;
+if(!a==ad)
+{
 var DATIME = (function(module)
 {
 	module.DoIt = function(resultObject)
 	{
-		resultObject.prepend(Date() + '<br />');
+		resultObject.prepend(Date() + '<.< Your' + a + '<br />');
 	};
 	return module;
 }(DATIME || {}));
+}
+else
+{
+	alert("Please put some SWAG.");
+}
